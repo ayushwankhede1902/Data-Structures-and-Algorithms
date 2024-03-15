c884a5fac41d0c29b99c33d4898e75c62cfcee82
ayushwankhede1902 <138423723+ayushwankhede1902@users.noreply.github.com>
2024-03-16 02:10:58 +0530

    Add files via upload

---
+++ Conditional Loops/while_loop.java
@@ -0,0 +1,34 @@
+public class while_loop {
+    public static void main(String[] args) {
+        //While Loops
+
+        /*
+          Syntax for while loop
+          while(condition){
+            //body
+          }
+         */
+        int num = 1;
+        while(num <= 6 ){
+            System.out.println(num);
+            num += 1;
+        }
+
+        // do while loop
+        /*
+           Syntax for Do While loop
+           do{
+
+           }while(condition);
+         */
+
+         int n = 1;
+         do{
+            System.out.println("Hello World");
+         } while( n!=1);
+         //Executes one time and then checks the condition
+         
+
+
+    }
+}
