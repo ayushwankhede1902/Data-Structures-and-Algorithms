1c28de8a24a83c97905964ef5816c832b9e932c8
ayushwankhede1902 <138423723+ayushwankhede1902@users.noreply.github.com>
2024-03-21 23:49:57 +0530

    Add files via upload

---
+++ Conditional Loops/Calculator.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Calculator {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Take input from the user till user does not press x or X
+        int ans = 0;
+
+        while(true){
+            //Take operator as input
+            System.out.println(("Enter the operator: "));
+            char op = input.next().trim().charAt(0);
+            System.out.println();
+
+            if ( op == '+' || op == '-' || op == '*' || op == '/' || op == '%'){
+                // input two numbers
+                System.out.println("Enter two numbers: ");
+                int num1 = input.nextInt();
+                int num2 = input.nextInt();
+
+                if ( op == '+'){
+                    ans = num1 + num2;
+                }
+                if ( op == '-'){
+                    ans = num1 - num2;
+                }
+                if ( op == '*'){
+                    ans = num1 * num2;
+                }
+                if ( op == '/'){
+                    if (num2!=0){
+                        ans = num1 / num2;
+                    }
+                }
+                if ( op == '%'){
+                    ans = num1 % num2;
+                }
+            
+                else if (op == 'x' || op == 'X'){
+                    break;
+                }
+                System.out.println(ans);
+            }
+        }
+    }
+}
