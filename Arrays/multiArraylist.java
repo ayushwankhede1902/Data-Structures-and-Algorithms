2b246a29f0597cabf2fb388e3df1e29c9674a81f
ayushwankhede1902 <138423723+ayushwankhede1902@users.noreply.github.com>
2024-05-29 22:46:25 +0530

    Add files via upload

---
+++ Arrays/multiArraylist.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class multiArraylist {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    ArrayList<ArrayList<Integer>> list =new ArrayList<>();
+
+    //Initialisation
+    for(int i = 0 ; i<3 ; i++){
+        list.add(new ArrayList<>());
+    }
+
+    //add elements
+    for(int i = 0 ; i < 3 ; i++){
+        for(int j = 0 ; j < 3 ; j++){
+            list.get(i).add(input.nextInt());
+        }
+    }
+    System.out.println(list);
+
+    }
+}
