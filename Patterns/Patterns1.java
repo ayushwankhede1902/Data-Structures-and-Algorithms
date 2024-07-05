3d84b25c3e636d4b072b4ef6dffa0f3c8af1dd67
Ayush Wankhede <138423723+ayushwankhede1902@users.noreply.github.com>
2024-07-05 09:23:36 +0530

    Add files via upload

---
+++ Patterns/Patterns1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Patterns1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = input.nextInt();
+        patt(n);
+    }
+    public static void patt(int n){
+         for(int i = 1 ; i <=n ; i++){
+            //for every row , run the column
+            for(int j = 1 ; j <= n ; j++){
+               System.out.print("* "); 
+            }
+            // When one row is printed we have to add new line
+            System.out.println();
+         }
+    }
+}
+
+// Output (for n = 4)
+// * * * * 
+// * * * *
+// * * * *
+// * * * *
