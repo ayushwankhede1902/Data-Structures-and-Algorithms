2554f1d23568072b8a4bb8dcca37ffbd82b4c95b
Ayush Wankhede <138423723+ayushwankhede1902@users.noreply.github.com>
2024-08-02 23:22:11 +0530

    Add files via upload

---
+++ Maths/BinarySearchSQRT.java
@@ -0,0 +1,41 @@
+public class BinarySearchSQRT {
+  public static void main(String[] args) {
+    int n = 40;
+    int p = 3;  // Precision value
+
+    System.out.printf("%.3f" ,sqrt(n,p));
+  }  
+  //Time : O(log(n))
+
+  public static double sqrt(int n , int p){
+    int s = 0;
+    int e = n;
+
+    double root = 0.0;
+
+    while(s < e){
+        int m = s +(e - s)/2;
+
+        if(m * m == n){
+            return m;
+        }
+        if(m * m > n){
+            e = m - 1;
+        }
+        else{
+            s = m + 1; 
+        }
+    }
+
+    // Run the loop till the time you need the precision value
+    double increment = 0.1;
+    for(int i = 0 ; i < p ; i++){
+        while(root * root <=n){
+            root += increment;
+        }
+        root -= increment;
+        increment /= 10;
+    }
+    return root;
+  }
+}
