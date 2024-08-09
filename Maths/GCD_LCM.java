9495b1e2331041291672439267d94d9868fe4ba3
Ayush Wankhede <138423723+ayushwankhede1902@users.noreply.github.com>
2024-08-09 10:53:33 +0530

    Add files via upload

---
+++ Maths/GCD_LCM.java
@@ -0,0 +1,17 @@
+public class GCD_LCM {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 12;
+        System.out.println(gcd(a,b));
+        System.out.println(lcm(a,b));
+    }
+    static int gcd(int a ,int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a , a);
+    }
+    static int lcm(int a , int b){
+        return a * b/gcd(a,b);
+    }
+}
