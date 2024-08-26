1af0178958d5ef6826799dc8b9b80e86bc6c9d86
Ayush Wankhede <138423723+ayushwankhede1902@users.noreply.github.com>
2024-08-26 23:04:24 +0530

    Add files via upload

---
+++ Maths/PowOfTwo.java
@@ -0,0 +1,7 @@
+public class PowOfTwo {
+    public static void main(String[] args) {
+        int n = 16; // Note : fix for n = 0
+        boolean ans =( n & (n-1)) == 0;
+        System.out.println(ans);
+    }
+}
